bf7091e03ef25f003f615d49cb8238f4bf84600d
Donny <1021001390@qq.com>
2018-04-16 15:57:24 +0800

    Init project

---
+++ src/views/layout/components/Mheader/Mheader.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="mheader">
+    <img src="" alt="">
+    <span>B军工作台</span>
+    <ul>
+      <li>
+        <img :src="userLogo" alt="" class="adminLogo">
+        <span>Admin</span>
+      </li>
+      <li>
+        <el-badge :value="20" :max="99" class="item">
+          <i class="el-icon-bell"></i>
+        </el-badge>
+      </li>
+      <li><i class="el-icon-error"></i></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import userLogo from '@/assets/img/u225.png'
+export default {
+  name: 'MheaderItem',
+  data() {
+    return {
+      userLogo
+    }
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  @import 'src/styles/mheader.scss';
+</style>
+
---
+++ src/views/layout/components/Sidebar/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <scroll-bar>
+    <div :class="['topTitle',isCollapse?'small':'big']">
+      <img :src="userLogo" alt="">
+      <a href="#/dashboard" v-if="!isCollapse">vue-admin</a>
+    </div>
+    <el-menu mode="vertical" unique-opened :default-active="$route.path" :collapse="isCollapse" background-color="#304156" text-color="#fff" active-text-color="#409EFF">
+      <sidebar-item :routes="routes"></sidebar-item>
+    </el-menu>
+  </scroll-bar>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import SidebarItem from './SidebarItem'
+import ScrollBar from '@/components/ScrollBar'
+import userLogo from '@/assets/img/u225.png'
+
+export default {
+  data() {
+    return {
+      userLogo
+    }
+  },
+  components: { SidebarItem, ScrollBar },
+  computed: {
+    ...mapGetters([
+      'sidebar'
+    ]),
+    routes() {
+      return this.$router.options.routes
+    },
+    isCollapse() {
+      return !this.sidebar.opened
+    }
+  }
+}
+</script>
+<style rel="stylesheet/scss" lang="scss" scoped>
+.topTitle{
+  height: 50px;
+  display: flex;
+  align-items: center;
+  &.big{
+    padding-left: 18px;
+  }
+  &.small{
+    padding-left: 4px;
+  }
+  a{
+    font-weight: bold;
+    padding-left: 10px;
+    color: white;
+    font-size: 18px;
+  }
+  img{
+    width: 25px;
+    height: 25px;
+    border-radius: 25px;
+  }
+}
+</style>
